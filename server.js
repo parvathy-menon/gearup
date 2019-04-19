03061bc8fefffd09909e34a1906b7f6969f71806
Parvathy Menon <parvathy.menon1997@gmail.com>
2019-04-18 18:19:03 -0700

    Added front-end

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 
-const app = express();
 const users = require("./routes/api/users");
 
+const app = express();
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
@@ -13,10 +13,8 @@ app.use(
   })
 );
 app.use(bodyParser.json());
-
 // DB Config
 const db = require("./config/keys").mongoURI;
-
 // Connect to MongoDB
 mongoose
   .connect(
@@ -33,6 +31,5 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
-const port = process.env.PORT || 5000;
-
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
